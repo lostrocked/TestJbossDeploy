f0af9ac0eccb524bd3ebb2b0fe39892d09427416
lostrocked <lostrocked@gmail.com>
2020-03-28 20:42:43 -0500

    Create vars_aws.tf

---
+++ template/vars_aws.tf
@@ -0,0 +1,26 @@
+variable "access_key" {
+  default = "AKIAJAWDGTHWN37HFJCQ"
+}
+
+variable "secret_key" {
+  default = "hxlAkuIxGenxOj3KyIcSLulQT7ClmdR78YHUOeJM"
+}
+
+variable "aws_key_name" {
+	default = "sachin-key-us-east-1"
+}
+
+variable "private_key_file" {
+	default = "jeny-key-us-east-1.pem"
+}
+variable "aws_ami" {
+	default = "ami-07ebfd5b3428b6f4d"
+}
+
+variable "aws_security_group_id" {
+	default = "sg-193b7469"
+}
+
+variable "email" {
+	default = "sachin.srivastava@broadcom.com"
+}
