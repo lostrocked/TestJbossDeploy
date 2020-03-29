97ef488f9f80c0c4c9308dde62f9216e1ff83efe
lostrocked <lostrocked@gmail.com>
2020-03-28 23:23:44 -0500

    Update main.tf

--- template/main.tf
+++ template/main.tf
@@ -9,8 +9,8 @@ resource "random_string" "password" {
 
 provider "aws" {
 	region     = "us-east-1"
-	access_key = "${var.access_key}"
-	secret_key = "${var.secret_key}"
+	access_key = "${var.aws_access_key}"
+	secret_key = "${var.aws_secret_key}"
 }
 
 resource "aws_instance" "cda_instance" {
