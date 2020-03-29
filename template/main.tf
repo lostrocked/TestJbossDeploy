65d824c27b3c868f7cb12074b30890a8548436bd
lostrocked <lostrocked@gmail.com>
2020-03-28 23:42:57 -0500

    Update main.tf

--- template/main.tf
+++ template/main.tf
@@ -1,3 +1,9 @@
+variable "aws_access_key" {default = "AKIAJAWDGTHWN37HFJCQ"}
+variable "aws_secret_key" {default = "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"}
+//variable "aws_ami" {default = "ami-0080e4c5bc078760e"}
+variable "aws_ami" {default = "ami-07ebfd5b3428b6f4d"}
+variable "aws_security_group_id" {default = "sg-193b7469"}
+variable "instance_type" {default = "t2.micro"}
 locals {
 	in_id = "${random_string.password.result}"
 }
