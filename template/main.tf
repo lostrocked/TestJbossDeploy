bac6452393911d27017d0e00387986d369e7bea8
lostrocked <lostrocked@gmail.com>
2020-07-05 19:48:26 -0500

    Update main.tf

--- template/main.tf
+++ template/main.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "cda_instance" {
 	ami                    = "ami-098f16afa9edf40be"
 	instance_type          = "t2.micro"
 	vpc_security_group_ids = [
-        			"subnet-9df4e8a3",
+        			"test-subnet",
         ]
 	key_name	= "Linux-Server-Private-Key"
 	subnet_id = "subnet-9df4e8a3"
